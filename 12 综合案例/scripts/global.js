f2eb3fc123ad042c52134b88ef60cd0c15b9c0ee
jiaqunhe <15104653258@163.com>
2018-07-24 11:42:03 +0800

    test

---
+++ 12 综合案例/scripts/global.js
@@ -0,0 +1,412 @@
+/**
+ * Created by dell on 2017/9/24.
+ */
+function addLoadEvent(func) {
+    var oldonload = window.onload;
+    if (typeof window.onload != 'function') {
+        window.onload = func;
+    } else {
+        window.onload = function() {
+            oldonload();
+            func();
+        }
+    }
+}
+
+function insertAfter(newElement,targetElement) {
+    var parent = targetElement.parentNode;
+    if (parent.lastChild == targetElement) {
+        parent.appendChild(newElement);
+    } else {
+        parent.insertBefore(newElement,targetElement.nextSibling);
+    }
+}
+
+function addClass(element,value) {
+    if (!element.className) {
+        element.className = value;
+    } else {
+        newClassName = element.className;
+        newClassName += " ";
+        newClassName += value;
+        element.className = newClassName;
+    }
+}
+
+function highlightPage() {
+    if (!document.getElementsByTagName) return false;
+    if (!document.getElementById) return false;
+    var headers = document.getElementsByTagName('header');
+    if (headers.length == 0) return false;
+    var navs = headers[0].getElementsByTagName('nav');
+    if (navs.length == 0) return false;
+    // 取得导航链接，然后循环遍历它们
+    var links = navs[0].getElementsByTagName("a");
+    var linkurl;
+    for (var i=0;i<links.length;i++) {
+        //比较当前链接的URL与当前页面的URL，比较字符串
+        linkurl = links[i].getAttribute("href");
+        if (window.location.href.indexOf(linkurl) != -1) {
+            links[i].className = "here";
+            var linktext = links[i].lastChild.nodeValue.toLowerCase();
+            document.body.setAttribute("id",linktext);
+        }
+    }
+}
+addLoadEvent(highlightPage);
+
+function moveElement(elementID,final_x,final_y,interval) {
+    if (!document.getElementById) return false;
+    if (!document.getElementById(elementID)) return false;
+    var elem = document.getElementById(elementID);
+    if (elem.movement) {
+        clearTimeout(elem.movement);
+    }
+    if (!elem.style.left) {
+        elem.style.left = "0px";
+    }
+    if (!elem.style.top) {
+        elem.style.top = "0px";
+    }
+    var xpos = parseInt(elem.style.left);
+    var ypos = parseInt(elem.style.top);
+    if (xpos == final_x && ypos == final_y) {
+        return true;
+    }
+    var dist = 0;
+    if (xpos < final_x) {
+        dist = Math.ceil((final_x - xpos)/10);
+        xpos = xpos + dist;
+    }
+    if (xpos > final_x) {
+        dist = Math.ceil((xpos - final_x)/10);
+        xpos = xpos - dist;
+    }
+    if (ypos < final_y) {
+        dist = Math.ceil((final_y - ypos)/10);
+        ypos = ypos + dist;
+    }
+    if (ypos > final_y) {
+        dist = Math.ceil((ypos - final_y)/10);
+        ypos = ypos - dist;
+    }
+    elem.style.left = xpos + "px";
+    elem.style.top = ypos + "px";
+    var repeat = "moveElement('"+elementID+"',"+final_x+","+final_y+","+interval+")";
+    elem.movement = setTimeout(repeat,interval);
+}
+
+function prepareSlideshow() {
+    if (!document.getElementsByTagName) return false;
+    if (!document.getElementById) return false;
+    if (!document.getElementById("intro")) return false;
+    //把幻灯片放到intro段落后面
+    var intro = document.getElementById("intro");
+    var slideshow = document.createElement("div");
+    slideshow.setAttribute("id","slideshow");
+
+    var preview = document.createElement("img");
+    preview.setAttribute("src","images/slideshow.jpg");
+    preview.setAttribute("alt","幻灯片");
+    preview.setAttribute("id","preview");
+    slideshow.appendChild(preview);
+    insertAfter(slideshow,intro);
+    //循环遍历intro中的所有链接，并根据鼠标所在链接来移动preview元素
+    var links = document.getElementsByTagName("a");
+    var destination;
+    for (var i=0;i<links.length;i++) {
+        links[i].onmouseover = function() {
+            destination = this.getAttribute("href");
+            if (destination.indexOf("index.html") != -1) {
+                moveElement("preview",0,0,5);
+            }
+            if (destination.indexOf("about.html") != -1) {
+                moveElement("preview",-150,0,5);
+            }
+            if (destination.indexOf("photos.html") != -1) {
+                moveElement("preview",-300,0,5);
+            }
+            if (destination.indexOf("live.html") != -1) {
+                moveElement("preview",-450,0,5);
+            }
+            if (destination.indexOf("contact.html") != -1) {
+                moveElement("preview",-600,0,5);
+            }
+        }
+    }
+}
+addLoadEvent(prepareSlideshow);
+
+//根据制定的id，显示相应的<section>，同时隐藏其他部分。
+function showSection(id) {
+    var sections = document.getElementsByTagName("section");
+    for (var i=0;i<sections.length;i++) {
+        if (sections[i].getAttribute("id") != id) {
+            sections[i].style.display = "none";
+        } else {
+            sections[i].style.display = "block";
+        }
+    }
+}
+//还需在<article>中的<nav>所包含的链接被单击时调用showSection函数
+function prepareInternalnav() {
+    if (!document.getElementsByTagName) return false;
+    if (!document.getElementById) return false;
+    var articles = document.getElementsByTagName("article");
+    if (articles.length == 0) return false;
+    var navs = articles[0].getElementsByTagName("nav");
+    if (navs.length == 0) return false;
+    var nav = navs[0];
+    var links = nav.getElementsByTagName("a");
+    for (var i=0;i<links.length;i++) {
+        //提取#后面的ID，数组的第一个元素是0，我们想要的第二个是1
+        var sectionId = links[i].getAttribute("href").split("#")[1];
+        //测试是否有相应ID的元素，如不存在，继续下次循环
+        if (!document.getElementById(sectionId)) continue;
+        //页面加载后，需要默认隐藏所有部分
+        document.getElementById(sectionId).style.display = "none";
+        //添加onclick函数，单击时，把sectionID传给showSection函数
+        links[i].destination = sectionId;
+        links[i].onclick = function () {
+            showSection(this.destination);
+            return false;
+        }
+    }
+}
+addLoadEvent(prepareInternalnav);
+
+//6、7章图片库函数
+function showPic(whichpic) {
+    if(!document.getElementById("placeholder")) return true;
+    var source = whichpic.getAttribute("href");
+    var placeholder = document.getElementById("placeholder");
+    placeholder.setAttribute("src",source);
+    if(!document.getElementById("description")) return false;
+    if(whichpic.getAttribute("title")) {
+        var text = whichpic.getAttribute("title");
+    }else {
+        var text = "";
+    }
+    var description = document.getElementById("description");
+    if(description.firstChild.nodeType == 3) {
+        description.firstChild.nodeValue = text;
+    }
+    return false;
+}
+
+function preparePlaceholder() {
+    if(!document.createElement) return false;
+    if(!document.createTextNode) return false;
+    if(!document.getElementById) return false;
+    if(!document.getElementById("imagegallery")) return false;
+    var placeholder = document.createElement("img");
+    placeholder.setAttribute("id","placeholder");
+    placeholder.setAttribute("src","images/div快速生成.png");
+    placeholder.setAttribute("alt","我的电影馆");
+    var description = document.createElement("p");
+    description.setAttribute("id","description");
+    var desctext = document.createTextNode("选择一张图片。");
+    description.appendChild(desctext);
+    var gallery = document.getElementById("imagegallery");
+    insertAfter(description,gallery);
+    insertAfter(placeholder,description);
+}
+
+function prepareGallery() {
+    if (!document.getElementsByTagName) return false;
+    if (!document.getElementById) return false;
+    if (!document.getElementById("imagegallery")) return false;
+    var gallery = document.getElementById("imagegallery");
+    var links = gallery.getElementsByTagName("a");
+    for (var i = 0; i < links.length; i++) {
+        links[i].onclick = function () {
+            return showPic(this);
+        }
+        links[i].onkeypress = links.onclick;
+    }
+}
+addLoadEvent(preparePlaceholder);
+addLoadEvent(prepareGallery);
+
+//第九章表格样式话函数
+function stripeTables() {
+    if (!document.getElementsByTagName) return false;
+    var tables = document.getElementsByTagName("table");
+    var odd,rows;
+    for (var i=0;i<tables.length;i++) {
+        odd = false;
+        rows = tables[i].getElementsByTagName("tr");
+        for (var j=0;j<rows.length;j++) {
+            if (odd == true) {
+                addClass(rows[j],"odd")
+                odd = false;
+            } else {
+                odd = true;
+            }
+        }
+    }
+}
+
+function highlightRows() {
+    if (!document.getElementsByTagName) return false;
+    var rows = document.getElementsByTagName("tr");
+    for (var i=0;i<rows.length;i++) {
+        rows[i].oldClassName = rows[i].className;
+        rows[i].onmouseover = function() {
+            addClass(this,"highlight");
+        }
+        rows[i].onmouseout = function() {
+            this.className = this.oldClassName;
+        }
+    }
+}
+
+function displayAbbreviations() {
+    //检查这几个方法是否存在
+    if (!document.getElementsByTagName) return false;
+    if (!document.createElement) return false;
+    if (!document.createTextNode) return false;
+    //或者 if(!document.getElementsByTagName || !document.createElement|| !document.createTextNode) return false;
+    //取得所有缩略词
+    var abbreviations = document.getElementsByTagName("abbr");
+    if (abbreviations.length < 1) return false;
+    var defs = [];
+    //遍历这些缩略词
+    for (var i=0;i<abbreviations.length;i++) {
+        var current_abbr = abbreviations[i];
+        if (current_abbr.childNodes.length < 1) continue;
+        var definition = current_abbr.getAttribute("title");
+        var key = current_abbr.lastChild.nodeValue;
+        defs[key] = definition;
+    }
+    //创建定义列表
+    var dlist = document.createElement("dl");
+    //遍历定义
+    for (key in defs) {
+        definition = defs[key];
+        //创建定义标题
+        var dtitle = document.createElement("dt");
+        var dtitle_text = document.createTextNode(key);
+        dtitle.appendChild(dtitle_text);
+        //创建定义描述
+        var ddesc = document.createElement("dd");
+        var ddesc_text = document.createTextNode(definition);
+        ddesc.appendChild(ddesc_text);
+        //把它们添加到定义列表
+        dlist.appendChild(dtitle);
+        dlist.appendChild(ddesc);
+    }
+    if (dlist.childNodes.length < 1) return false;
+    //创建标题
+    var header = document.createElement("h3");
+    var header_text = document.createTextNode("Abbreviations");
+    header.appendChild(header_text);
+    //把标题添加到页面主体
+    var articles = document.getElementsByTagName("article");
+    if (articles.length == 0) return false;
+    var container = articles[0];
+    container.ppendChild(header);
+    //把定义列表添加到页面主体
+    container.appendChild(dlist);
+}
+addLoadEvent(stripeTables);
+addLoadEvent(highlightRows);
+addLoadEvent(displayAbbreviations);
+
+//如果label中的文本被单击，关联的表单字段就会获得焦点。
+//在浏览器不支持的情况下，我们自己添加这种行为。
+function focusLabels() {
+    if (!document.getElementsByTagName) return false;
+    var labels = document.getElementsByTagName("label");
+    for (var i=0;i<labels.length;i++) {
+        //遍历，如果label有for属性，添加一个事件处理函数
+        if (!labels[i].getAttribute("for")) continue;
+        //在label被单击时，提取for属性的值，就是相应表单字段的ID值
+        labels[i].onclick = function () {
+            var id = this.getAttribute("for");
+            if (!document.getElementById(id)) return false;
+            var element = document.getElementById(id);
+            element.focus();
+        }
+    }
+}
+addLoadEvent(focusLabels);
+
+//为元素获得焦点的事件添加一个处理函数；再为失去焦点的事件添加一个处理函数。
+function resetFields(whichform) {
+    if (Modernizr.input.placeholder) return;
+    for (var i=0;i<whichform.elements.length;i++) {
+        var element = whichform.elements[i];
+        if (element.type == "submit") continue;
+        var check = element.placeholder || element.getAttribute('placeholder');
+        if (!check) continue;
+        element.onfocus = function () {
+            var text = this.placeholder || this.getAttribute('placeholder');
+            if (this.value == text) {
+                this.className = '';
+                this.value = "";
+            }
+        }
+        element.onblur = function () {
+            if (this.value == "") {
+                this.className = 'placeholder';
+                this.value = this.placeholder || this.getAttribute('placeholder');
+            }
+        }
+        element.onblur();
+    }
+}
+function isFilled(field) {
+    if (field.value.replace(' ','').length == 0) return false;
+    var placholder = field.placeholder || field.getAttribute('placeholder');
+    return (field.value != pladeholder);
+}
+function isEmail(field) {
+    return (field.value.indexOf("@") != -1 && field.value.indexOf(".") != -1);
+}
+function validateForm(whichform) {
+    for (var i=0;i<whichform.elements.length;i++) {
+        var element = whichform.elements[i];
+        if (element.required == 'required') {
+            if (!isFilled(element)) {
+                alert("请填写"+element.name);
+                return false;
+            }
+        }
+        if (element.type == 'email') {
+            if (!isEmail(element)) {
+                alert(element.name+"必须是一个正确的邮箱地址。");
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+//传入form对象调用resetFields函数
+function prepareForms() {
+    for (var i=0;i<document.forms.length;i++) {
+        var thisform = document.forms[i];
+        resetFields(thisform);
+        thisform.onsubmit = function () {
+            return validateForm(this);
+        }
+    }
+}
+addLoadEvent(prepareForms);
+
+function getHTTPObject() {
+    if(typeof XMLHttpRequest == "undefined")
+        XMLHttpRequest = function() {
+            try{ return new ActiveXObject("Msxml2.XMLHTTP.6.0");}
+            catch (e) {}
+            try{ return new ActiveXObject("Msxml2.XMLHTTP.3.0");}
+            catch (e) {}
+            try{ return new ActiveXObject("Msxml2.XMLHTTP");}
+            catch (e) {}
+            return false;
+        }
+    return new XMLHttpRequest();
+}
+
+
